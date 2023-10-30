4b332bcb606158124be8e25385c5feedf509362f
Luisa Rudin <e227449@edu.sbl.ch>
2023-10-30 11:44:05 +0100

    Aufgabe zwei Funktion fertig

--- script.js
+++ script.js
@@ -4,7 +4,38 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
+ // Wenn das aktuelle Zeichen ein 'e' ist, hänge nichts an die
+if (currentElement === 'e') {
+  result.push("")
+  // Wenn das aktuelle Zeichen 'E' ist, hänge nichts an die
+}else if ( currentElement === 'E'){
+
+}else {
+  result.push(currentElement)
+}
+
   }
   return result.join("")
 }
+
+
+
+
+
+export function aufgabe02(args) {
+  const input = args
+  const result = []
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    // Wandle das aktuelle Element in enen Grossbuchstaben um
+const capitalizedElement = currentElement.toUpperCase()
+
+//Hänge den Grossbuchstaben an das Resultat an.
+result.push(capitalizedElement)
+
+
+  }
+  return result.join("")
+}
+
